6e28c7afcef8a003f2f3390508425520656a61e9
Vladimir Valuikin <vv.valuikin@gmail.com>
2019-11-28 17:43:59 +0300

    ChangeNumber2

--- src/MyArrayList/Test.java
+++ src/MyArrayList/Test.java
@@ -11,9 +11,10 @@ public class Test {
         list.add("c");
         list.add("d");
 
-        list.remove(-1);
+        list.remove(1);
         list.remove(2);
         list.remove(0);
+        list.changeNumber2();
     }
 
     private static void check(boolean condition, String message) {
@@ -83,6 +84,10 @@ public class Test {
 
         public int size() {
             return array.length;
+        }
+
+        public void changeNumber2 (){
+            System.out.println("Norm");
         }
     }
 }
